d5733cb77d08d4c888ddeb323d76dbd2e19eb1f2
NaughtySora <naughtysora@proton.me>
2026-06-26 22:07:16 +0300

    test for var load with types

--- test/env.js
+++ test/env.js
@@ -3,31 +3,48 @@
 const { loadEnvFile, env } = require('node:process');
 const { describe, it } = require("node:test");
 const { load } = require("../lib/env.js");
+const { Buffer } = require('node:buffer');
 const assert = require('node:assert/strict');
-const { reflection } = require('naughty-util');
 loadEnvFile('.env');
 
-describe.only('env', () => {
-  it('load', () => {
-    // 0. HTTP_PORT=3000 (skipped) no prefix
-    // 1. APP:INT_PORT=3000 (ok, to int)
-    // 2. APP_HTTP_PORT=3000 (ok, no parsing)
-    // 3. APP:I_HTTP_PORT=3000 (ok, invalid parser key)
-    // 4. APP:I_HTTP_HTTP_PORT=3000 (ok, invalid parser key)
-    // 5. APP:INT_HTTP_HTTP_HTTP_PORT=3000 (ok, to int)
-    const envs = load('APP');
-    assert.deepEqual(envs,
+describe('env - load', () => {
+  it('simple', () => {
+    assert.deepEqual(load('APP'),
       {
         http: {
           http: {
             http: {
-              port: 3000 // 5.
+              port: 3000
             },
-            port: '3000' // 4.
+            port: '3000'
           },
-          port: '3000' // 2. 3.
+          port: '3000'
         },
-        port: 3000 // 1.
+        port: 3000
       });
+  });
+
+  it('with types', () => {
+    assert.deepEqual(load('$'),
+      {
+        bigint: 12456842348230948230948230942309489234832904823094n,
+        answer: 42,
+        http: { debug: true },
+        service2: { secret: Buffer.from('018d49ff7f7cedf0edf17d90d224d6f9', 'hex') },
+        service1: { secret: Buffer.from('40bBaUZ+q7c/WMv3GlnHHA==', 'base64') },
+        service0: {
+          secret: Buffer.from('unicode_secret_for_some_reason', 'utf-8'),
+        },
+        service3: {
+          secret: Buffer.from('_3md6vtKJE2vBrfE7XX7rg', 'base64url'),
+        },
+        deep: { float: { value: 33.35123 } },
+        hex: 3735928559,
+        size: 33,
+        user: { address: { street: 'abc 12', city: 'New York', Country: 'USA' } },
+        nullabla: { '1': null, '2': undefined },
+        oct: { value: 493 }
+      },
+    );
   });
 });
